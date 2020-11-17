09851dac11873b0891f4b06bb869733d95252b98
acarbf01 <acarbf01@estudiantes. unileon.es>
2020-11-17 20:58:00 +0100

    Final

--- src/es/unileon/prg/date/Date.java
+++ src/es/unileon/prg/date/Date.java
@@ -229,4 +229,172 @@ public class Date {
 		}
 		return salida.toString();
 	}
+
+    public String PrintDate(){
+        return this.day +" de "+this.PrintMonthName()+" de "+this.year;
+	}
+	
+	public String PrintDays(){
+		StringBuffer salida=new StringBuffer();
+		int numDay;
+		switch (this.month) {
+			case 1: //next
+			case 3: //next
+			case 5: //next
+			case 7: //next
+			case 8: //next
+			case 10: //next
+			case 12:
+				numDay = 31;
+				break;
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				numDay = 30;
+				break;
+			case 2:
+				numDay = 28;
+				break;			
+			}
+		for(int i=0; i < numDay-this.day; i++){
+			
+			salida.append((this.day+i)+"/"+(this.month)+"/"+(this.year));
+		}
+		return salida.toString();
+	}
+
+	public String PrintMonthsWSDays(){
+		StringBuffer salida = new StringBuffer(this.PrintMonthName()+" tiene el mismo numero de dias que ");
+
+		switch(this.getMonth()){
+				case 1:
+					salida.append("Marzo, Mayo, Julio, Agosto, Octubre y Diciembre");
+					break;
+			
+				case 2:
+					salida.append("ningun otro mes");
+					break;
+				
+				case 3:
+					salida.append("Enero, Mayo, Julio, Agosto, Octubre y Diciembre");
+					break;
+					
+				case 4:
+					salida.append("Junio, Septiembre y Noviembre");
+					break;
+			
+				case 5:
+					salida.append("Enero, Marzo, Julio, Agosto, Octubre y Diciembre");
+					break;
+				
+				case 6:
+					salida.append("Abril, Septiembre y Noviembre");
+					break;
+					
+				case 7:
+					salida.append("Enero, Marzo, Mayo, Agosto, Octubre y Diciembre");
+					break;
+			
+				case 8:
+					salida.append("Enero, Marzo, Mayo, Junio, Octubre y Diciembre");
+					break;
+				
+				case 9:
+					salida.append("Abril, Junio y Noviembre");
+					break;
+					
+				case 10:
+					salida.append("Enero, Marzo, Mayo, Junio, Agosto y Diciembre");
+					break;
+			
+				case 11:
+					salida.append("Abril, Junio y Septiembre");
+					break;
+				
+				case 12:
+					salida.append("Enero, Marzo, Mayo, Junio, Agosto y Octubre");
+					break;
+		}
+		return salida.toString();
+	}
+
+	public int daysPast(){
+		int numDay, daysSinceYearBegin = 0;
+		for (int i=1;i<this.month;i++ ) {
+			switch(i){
+				case 1: //next
+				case 3: //next
+				case 5: //next
+				case 7: //next
+				case 8: //next
+				case 10:
+					numDay= 31;
+					break;
+				
+					case 2:
+					numDay = 28;
+					break;
+				
+				default:
+					numDay = 30;
+			}
+			daysSinceYearBegin = daysSinceYearBegin + numDay;
+		}
+		daysSinceYearBegin = daysSinceYearBegin + this.day;
+
+		return daysSinceYearBegin;
+	}
+
+	
+	public int attemptsToDate(){
+		int randomDay = 0, intentos = 0;
+		while(this.daysPast()!=randomDay){
+			randomDay = (int)(Math.random() * ((365 - 1) + 1)) + 1;
+			intentos++;
+		}
+		return intentos;
+	}
+
+	
+	public int attemptsToDateDoWhile(){
+		int randomDay = 0, intentos = 0;
+		do{
+			randomDay = (int)(Math.random() * ((365 - 1) + 1)) + 1;
+			intentos++;
+		}
+		while(this.daysPast()!=randomDay);
+		return intentos;
+	}
+
+	
+	public String dayOfTheWeek(int firstDayOfTheYear){
+		int dayOfTheWeek = (this.daysPast()%7)+firstDayOfTheYear; 
+		StringBuffer salida = new StringBuffer("El "+this.PrintDate()+" es ");
+		switch (dayOfTheWeek){
+			case 1:
+				salida.append("Lunes");
+				break;
+			case 2:
+				salida.append("Martes");
+				break;
+			case 3:
+				salida.append("Miercoles");
+				break;
+			case 4:
+				salida.append("Jueves");
+				break;
+			case 5:
+				salida.append("Viernes");
+				break;
+			case 6:
+				salida.append("Sabado");
+				break;
+			case 7:
+				salida.append("Domingo");
+				break;																																			
+		}
+
+		return salida.toString();
+	}
 }
--- src/es/unileon/prg/date/MainDate.java
+++ src/es/unileon/prg/date/MainDate.java
@@ -12,6 +12,14 @@ public class MainDate {
 			System.out.println(today.PrintMonthName());
 			System.out.println(today.PrintSeasonName());
 			System.out.println(today.PrintMonths());
+			System.out.println(today.PrintDate());
+			System.out.println(today.PrintDays());
+			System.out.println(today.PrintMonthsWSDays());
+			System.out.println("Han pasado "+today.daysPast()+" dias");
+			System.out.println(today.attemptsToDate()+"intentos");
+			System.out.println(today.attemptsToDateDoWhile()+" intentos ");
+			int primerDiaAnio = 1;
+			System.out.println(today.dayOfTheWeek(primerDiaAnio));
 		} catch (DateException e) {
 			System.out.println(e.getMessage());
 		}
